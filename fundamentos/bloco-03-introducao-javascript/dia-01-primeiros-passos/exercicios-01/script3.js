fcfca087278a7857eed74d8264898101c8f1fed3
Thiesa <thiesa.c@gmail.com>
2023-01-23 14:01:47 -0400

    Exercícios do dia

---
+++ fundamentos/bloco-03-introducao-javascript/dia-01-primeiros-passos/exercicios-01/script3.js
@@ -0,0 +1,11 @@
+//Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+let value = -9;
+
+if (value < 0) {
+  console.log("negative");
+} else if (value > 0) {
+  console.log("positive");
+} else if (value === 0) {
+  console.log("zero");
+}
