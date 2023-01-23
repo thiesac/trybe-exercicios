09f6c5a42dc79e31e4e58cf36d4796f82de6cb54
Thiesa <thiesa.c@gmail.com>
2023-01-23 18:29:04 -0400

    Exercício lucro

---
+++ fundamentos/bloco-03-introducao-javascript/dia-01-primeiros-passos/exercicios-01/script9.js
@@ -0,0 +1,7 @@
+const cost = 200;
+const tax = (cost * 20) / 100;
+const totalCost = cost + tax;
+const salesValue = 250;
+const profit = salesValue - totalCost;
+const companyProfit = profit * 1000;
+console.log(companyProfit);
