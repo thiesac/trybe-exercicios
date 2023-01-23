93559b5fb79e4704495449ac25e6048d52ac67be
Thiesa <thiesa.c@gmail.com>
2023-01-23 17:48:59 -0400

    Exercício ímpar

---
+++ fundamentos/bloco-03-introducao-javascript/dia-01-primeiros-passos/exercicios-01/script7.js
@@ -0,0 +1,9 @@
+const number1 = 7;
+const number2 = 6;
+const number3 = 7;
+
+if (number1 % 2 === 0 || number2 % 3 === 0 || number3 % 2 === 0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
