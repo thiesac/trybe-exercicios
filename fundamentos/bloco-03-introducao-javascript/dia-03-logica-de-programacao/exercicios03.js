32e71c0c9efb794db76e7d7cf5fa2be2ec9643c5
Thiesa <thiesa.c@gmail.com>
2023-01-25 13:51:52 -0400

    Terminando exercício palavra mais longe/curta

--- fundamentos/bloco-03-introducao-javascript/dia-03-logica-de-programacao/exercicios03.js
+++ fundamentos/bloco-03-introducao-javascript/dia-03-logica-de-programacao/exercicios03.js
@@ -1,10 +1,20 @@
 //imprimir a maior e a menor palavra do array
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let longest = 0;
+let array = ["java", "javascript", "python", "html", "css"];
+let longest = array[0];
+let smallest = array[0];
 
 for (let i = 0; i < array.length; i += 1) {
-    if (array[i].length > longest) {
-        longest = array[i]
-    }
+  let word = array[i].length;
+  if (word > longest.length) {
+    longest = array[i];
+  }
 }
-console.log(longest);   
+console.log("A palavra mais longe é: " + longest);
+
+for (let i = 0; i < array.length; i += 1) {
+  let word = array[i].length;
+  if (word < smallest.length) {
+    smallest = array[i];
+  }
+}
+console.log("A palavra mais curta é: " + smallest);
