bd8014f058fb6af394f5287480fbfeeef2bfe59b
Thiesa <thiesa.c@gmail.com>
2023-01-25 14:41:15 -0400

    exercício bônus 01

--- fundamentos/bloco-03-introducao-javascript/dia-03-logica-de-programacao/bonus01.js
+++ fundamentos/bloco-03-introducao-javascript/dia-03-logica-de-programacao/bonus01.js
@@ -1,7 +1,11 @@
 //imprimir um quadrado de asterisco do tamanho de "n"
 let number = 4;
-let character = '*'
+let character = "*";
+let line = "";
 
 for (let i = 0; i < number; i += 1) {
-    console.log(character);
+  line += character;
+}
+for (let i = 0; i < number; i += 1) {
+  console.log(line);
 }
