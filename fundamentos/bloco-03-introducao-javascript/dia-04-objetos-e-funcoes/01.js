0bf716c6c3e6608176ba07c3c1a694b81a578b02
Thiesa <thiesa.c@gmail.com>
2023-01-26 12:40:52 -0400

    Primeiros exercícios do dia

---
+++ fundamentos/bloco-03-introducao-javascript/dia-04-objetos-e-funcoes/01.js
@@ -0,0 +1,7 @@
+//imprimir mensagem de boas-vindas para a personagem
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+console.log("Bem-vinda, " + info.personagem);
