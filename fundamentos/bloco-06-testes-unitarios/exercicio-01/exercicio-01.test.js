65f604ac71268b3d23ba4dd8a09c95b5b2b3c251
Thiesa <thiesa.c@gmail.com>
2023-02-17 17:35:22 -0400

    exercícios do dia

---
+++ fundamentos/bloco-06-testes-unitarios/exercicio-01/exercicio-01.test.js
@@ -0,0 +1,7 @@
+const myRemove = require('./exercicio-01');
+test('myRemove() receives an array arr and returns a copy of this array without the element item, in case it is in the array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+})
+
