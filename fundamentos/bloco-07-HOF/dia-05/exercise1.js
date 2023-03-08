afe01a7c79c1064ec755158c545b177b310dbf15
Thiesa <thiesa.c@gmail.com>
2023-03-08 14:48:01 -0400

    Exercícios 1 e 2

---
+++ fundamentos/bloco-07-HOF/dia-05/exercise1.js
@@ -0,0 +1,8 @@
+// Escreva a função swap, que, dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados
+
+const myList = [5, 2, 3];
+
+// escreva swap abaixo
+const swap = ([num1, num2, num3]) => [num3, num2, num1];
+
+console.log(swap(myList));
