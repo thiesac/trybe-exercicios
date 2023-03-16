15ef9d5be1efcc364472b04e03327f36166c3a2c
Thiesa <thiesa.c@gmail.com>
2023-03-16 14:15:42 -0400

    Exercícios finais de fundamentos

--- fundamentos/bloco-07-HOF/dia-05/exercise3.js
+++ fundamentos/bloco-07-HOF/dia-05/exercise3.js
@@ -1,6 +1,6 @@
 // Escreva uma função que recebe o nome de uma pessoa e retorna uma mensagem de cumprimento
 
-const greet = (name, ...message) => `${name} ${message}`
+const greet = (name, message) => `${name} ${message}`
 
 // Retornos esperados:
 console.log(greet('John')); // 'Hi John'
