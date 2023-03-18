1b913a106b145cec043a9b3ef0edc97898915ffb
Thiesa <thiesa.c@gmail.com>
2023-03-18 15:38:32 -0400

    Exercício dia 2

--- front-end/secao-01-introducao-ao-frontend/dia-02-promises-e-fetch/src/main.js
+++ front-end/secao-01-introducao-ao-frontend/dia-02-promises-e-fetch/src/main.js
@@ -1,7 +1,20 @@
-fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
-  .then((response) => response.json())
-    .then((data) => {
-        // console.log(data.length);
-        const generateNumber = Math.floor(Math.random() * data.length)
-        console.log(generateNumber);
-  });
+const shuffleBtn = document.getElementById('suffle-btn');
+const heroName = document.getElementById('hero-name');
+const heroImg = document.getElementById('img-hero');
+const maxNumber = 731;
+const generateNumber = () => Math.floor(Math.random() * maxNumber);
+const URL = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/';
+
+const getHero = () => {
+  const shuffledId = generateNumber();
+  fetch(`${URL}${shuffledId}.json`)
+    .then((response) => response.json())
+      .then(({ name, images }) => {
+        heroName.innerHTML = name
+        heroImg.src = images.sm
+      })
+  .catch()
+
+}
+
+shuffleBtn.addEventListener('click', getHero)
